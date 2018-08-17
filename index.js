c1c5047e314a10ca17ebf58727aaefb1ed1e90de
Kelsey Green <kelseygreen@Kelseys-MacBook-Pro.local>
2018-08-17 08:37:10 -0700

    added css file and team page styling

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/'));
 
 
 app.get('/', function(req, res) {
